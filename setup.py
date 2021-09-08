4f8c8aadd3aaabe22479f49a7abc74cda9af1ae6
Miwa Daiki <miwa.daiki.mllab.nit@gmail.com>
2021-09-08 23:14:54 +0900

    first commit

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='selective_inference',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='tools for parametric selective inference',
     long_description=readme,
-    author='Kenneth Reitz',
+    author='Daiki Miwa',
     author_email='me@kennethreitz.com',
     url='https://github.com/kennethreitz/samplemod',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('numpy', 'portion', 'scipy.stats'))
 )
 
